d1308f27c87a37ef54cb162b789a96de12951035
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-04-23 13:28:07 -0300

    Merge Back master

--- MemberProducerSync/Controllers/MemberLegacyController.cs
+++ MemberProducerSync/Controllers/MemberLegacyController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MemberProducerSync.EF;
 using MemberProducerSync.MemberService;
 using MemberProducerSync.Model;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemberProducerSync.Controllers
