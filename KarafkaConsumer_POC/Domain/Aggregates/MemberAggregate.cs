6b7202212be3bb28c2a9919998ea6e29d86215f9
Matheus Primo <matheussilvaprimo@hotmail.com>
2018-04-24 16:03:17 -0300

    lil fix

--- KarafkaConsumer_POC/Domain/Aggregates/MemberAggregate.cs
+++ KarafkaConsumer_POC/Domain/Aggregates/MemberAggregate.cs
@@ -19,8 +19,8 @@ namespace KarafkaConsumer_POC.Domain.Aggregates
             Events.OrderBy(x => x.EventDate).ToList().ForEach(x =>
             {
                 if (!string.IsNullOrWhiteSpace(x.ID)) { Member.ID = x.ID; }
-                if (!string.IsNullOrWhiteSpace(x.ID)) { Member.LegacyID = x.LegacyID; }
-                if (!string.IsNullOrWhiteSpace(x.ID)) { Member.CellNumber = x.CellNumber; }
+                if (!string.IsNullOrWhiteSpace(x.LegacyID)) { Member.LegacyID = x.LegacyID; }
+                if (!string.IsNullOrWhiteSpace(x.CellNumber)) { Member.CellNumber = x.CellNumber; }
                 if (!string.IsNullOrWhiteSpace(x.FullName)) { Member.FullName = x.FullName; }
                 if (x.Age > 0) { Member.Age = x.Age; }
                 if (x.DateOfBirth != null || x.DateOfBirth != DateTime.MinValue) { Member.DateOfBirth = x.DateOfBirth; }
